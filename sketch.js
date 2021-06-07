8502dd85ea7da272b5a4b8f23404fba77af5a4a3
Saruwhitehatjr <sarurobin@gmail.com>
2021-06-07 15:00:02 +0530

    first commit

--- sketch.js
+++ sketch.js
@@ -29,6 +29,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    platform= new Ground(150,300,300,170);
 
 }
 
@@ -54,4 +55,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
